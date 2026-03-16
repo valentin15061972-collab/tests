43dc22f7c130dbbabfd114076275652a5724b929
Valentin <valentin15061972@gmail.com>
2026-03-16 17:42:48 +0300

    Create test_task1.py

---
+++ test_task1.py
@@ -0,0 +1,12 @@
+import pytest
+from src.task1 import check_age
+
+
+@pytest.mark.parametrize('age, expected',
+                        ((15, 'Доступ запрещён'),
+                        (25, 'Доступ разрешён'))
+)
+
+
+def test_check_age(age, expected):
+    assert check_age(age) == expected, 'Ошибка'
